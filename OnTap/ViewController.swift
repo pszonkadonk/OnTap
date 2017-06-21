1f7e421e2a63400e8fae780107d4f38645fd6bfc
Michael Pszonka <pszonkadonk@Michaels-Air.home>
2017-06-20 22:54:07 -0400

    initial commit

--- OnTap/ViewController.swift
+++ OnTap/ViewController.swift
@@ -9,16 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    
+    var menuShowing: Bool = false
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func openMenu(_ sender: Any) {
+        
+        if(menuShowing) {
+            leadingConstraint.constant = -161
+            menuShowing = false
+        } else {
+            leadingConstraint.constant = 0
+            menuShowing = true
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+        
+        
+        
     }
+
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
