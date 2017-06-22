618185d9abfe3022943c89eea3708330f44ca2bc
Michael Pszonka <pszonkadonk@Michaels-Air.home>
2017-06-21 23:02:10 -0400

    work with configuring json, allow http requests in info.plist

--- OnTap/ViewController.swift
+++ OnTap/ViewController.swift
@@ -38,6 +38,32 @@ class ViewController: UIViewController {
         
         
     }
+    @IBAction func getData(_ sender: Any) {
+        guard let url = URL(string: "http://api.brewerydb.com/v2/beer/oeGSxs?key=6ac28fb2b6b8ea4081184e492e5462d8") else {return}
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) {(data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                print(data)
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+                        if let beerData = json["data"] as? [String:Any] {
+                            print(beerData["abv"] as! String)
+                            if let beerStyle = beerData["style"] as? [String:Any] {
+                                print(beerStyle["description"] as! String)
+                            }
+                        }
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+            }.resume()
+    }
 
 //    override func didReceiveMemoryWarning() {
 //        super.didReceiveMemoryWarning()
